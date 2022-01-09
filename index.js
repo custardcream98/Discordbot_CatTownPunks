fc30d902ca556af7edfd15243329561c00e5f337
Shi Woo, Park <custardcream@kakao.com>
2022-01-10 03:44:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "DIRECT_MESSAGES"
+    ],
+    partials: [
+        "CHANNEL"
+    ]
+});
 const fs = require('fs')
 client.login(process.env.TOKEN);// 토큰 수정
 
