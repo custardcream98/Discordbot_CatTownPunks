5a99799cf3953801ce52fae4a4736cae334ebd08
Shi Woo, Park <custardcream@kakao.com>
2022-01-10 06:17:30 +0900

    Solve Problem caused by .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "DIRECT_MESSAGES"
+    ],
+    partials: [
+        "CHANNEL"
+    ]
+});
 const fs = require('fs')
 const { token, clientId } = require('./config.json');
 
